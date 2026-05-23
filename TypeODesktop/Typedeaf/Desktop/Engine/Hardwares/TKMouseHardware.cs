997539a8c3109a1981b30a2ce4fd09229bbd3fe1
Udrian <simonhaggren@gmail.com>
2026-05-23 20:48:36 +0200

    Implemented TK mouse input

--- TypeODesktop/Typedeaf/Desktop/Engine/Hardwares/TKMouseHardware.cs
+++ TypeODesktop/Typedeaf/Desktop/Engine/Hardwares/TKMouseHardware.cs
@@ -1,20 +1,30 @@
-﻿using TypeOEngine.Typedeaf.Core.Common;
+﻿using OpenTK.Windowing.GraphicsLibraryFramework;
+using TypeOEngine.Typedeaf.Core.Common;
 using TypeOEngine.Typedeaf.Core.Engine.Hardwares;
+using TypeOEngine.Typedeaf.Core.Interfaces;
 using TypeOEngine.Typedeaf.Desktop.Engine.Hardwares.Interfaces;
+using TypeOEngine.Typedeaf.TK;
+using TypeOEngine.Typedeaf.TK.Engine.Services;
 
 namespace TypeOEngine.Typedeaf.Desktop
 {
     namespace Engine.Hardwares
     {
-        internal class TKMouseHardware : Hardware, IMouseHardware
+        internal class TKMouseHardware : Hardware, IMouseHardware, IUpdatable
         {
+            private TKGameService GameService { get; set; }
+
+            private MouseState CurrentState { get; set; }
+
             public Vec2 CurrentMousePosition { get; set; }
             public Vec2 OldMousePosition { get; set; }
             public Vec2 CurrentWheelPosition { get; set; }
             public Vec2 OldWheelPosition { get; set; }
+            public bool Pause { get; set; }
 
             protected override void Initialize()
             {
+
             }
 
             protected override void Cleanup()
@@ -23,22 +33,37 @@ namespace TypeOEngine.Typedeaf.Desktop
 
             public bool CurrentButtonDownEvent(object key)
             {
-                return true;
+                return CurrentState.IsButtonDown((MouseButton)key);
             }
 
             public bool CurrentButtonUpEvent(object key)
             {
-                return true;
+                return CurrentState.IsButtonReleased((MouseButton)key); ;
             }
 
             public bool OldButtonDownEvent(object key)
             {
-                return true;
+                return CurrentState.WasButtonDown((MouseButton)key);
             }
 
             public bool OldButtonUpEvent(object key)
             {
-                return true;
+                return !CurrentState.WasButtonDown((MouseButton)key);
+            }
+
+            public void Update(double dt)
+            {
+                TKGameWindow game = GameService.TKGames.FirstOrDefault();
+                
+                if(game != null)
+                {
+                    CurrentState = game.MouseState.GetSnapshot();
+
+                    CurrentMousePosition = new Vec2(CurrentState.Position.X, CurrentState.Position.Y);
+                    OldMousePosition = new Vec2(CurrentState.PreviousPosition.X, CurrentState.PreviousPosition.Y);
+                    CurrentWheelPosition = new Vec2(CurrentState.Scroll.X, CurrentState.Scroll.Y);
+                    OldWheelPosition = new Vec2(CurrentState.PreviousScroll.X, CurrentState.PreviousScroll.Y);
+                }
             }
         }
     }
